f22aa7d35a2c11d8d282a8b8a2f0d40034ca6b48
s3miOtics <colby.kauk@gmail.com>
2023-02-25 14:24:53 -0800

    header component css and responsive design

--- react_portfolio_website/src/components/header/Header.jsx
+++ react_portfolio_website/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.PNG'
+import ME from '../../assets/meme.png'
 import HeaderSocials from './HeaderSocials'
 import { LINKEDIN } from '../../utils/constants/socialLinks'
 
@@ -9,7 +9,7 @@ import { LINKEDIN } from '../../utils/constants/socialLinks'
 const Header = () => {
   return (
     <header>
-      <div className="container header-container">
+      <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1 className="header-title">
           <a href={ LINKEDIN }>Colby Kauk</a>
@@ -17,7 +17,7 @@ const Header = () => {
         <h5 className="headline text-light ">Full-stack Developer</h5> 
         <CTA />
         <HeaderSocials />
-        <div>
+        <div className='me'>
           <img src={ME} alt="head shot" />
         </div>
         <a href='#contact' className='scroll_down'>Scroll Down</a>
