e901bcec0add67046c9d84ca819c05aba5820c06
s3miOtics <colby.kauk@gmail.com>
2023-02-27 15:08:28 -0800

    managing Nav states using useState Hook

--- react_portfolio_website/src/components/nav/Nav.jsx
+++ react_portfolio_website/src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./nav.css"
 import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
@@ -7,13 +7,16 @@ import { RiServiceLine } from 'react-icons/ri'
 import { BiMessageSquareDetail } from 'react-icons/bi'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState("#header")
+
   return (
     <nav>
-      <a href="#header"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#services"><RiServiceLine /></a>
-      <a href="#contact"><BiMessageSquareDetail /></a>
+      <a href="#header"  onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
     </nav>
   )
 }
