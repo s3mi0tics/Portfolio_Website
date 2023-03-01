d63140d9606013f509c2d4eb06f4c8d36bde46d4
s3miOtics <colby.kauk@gmail.com>
2023-03-01 14:19:11 -0800

    create content of experience.jsx file

--- react_portfolio_website/src/components/experience/Experience.jsx
+++ react_portfolio_website/src/components/experience/Experience.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
 import './experience.css'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="contianer experience_container">
+
+{/* ====================== /* Frontend Experience ===================== */}
+        <div className="experience_fronend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>Java</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+          </div>
+        </div>
+        
+{/* ============ Backend Experience ============= */}
+        <div className="experience_backend">
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>MongoDB</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>MySQL</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>Node.js</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>Java</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+            <article className="experience_details">
+              <BsFillPatchCheckFill />
+              <h4>Python</h4>
+              <small className='text-light'>Experienced</small>    
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
