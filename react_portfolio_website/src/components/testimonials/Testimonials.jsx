aae6648fd93937a024e5ca07840e278b5e903031
s3miOtics <colby.kauk@gmail.com>
2023-03-05 23:02:23 -0800

    make testimonials dynamic with data and use swiper

--- react_portfolio_website/src/components/testimonials/Testimonials.jsx
+++ react_portfolio_website/src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './testimonials.css'
-import AVTR1 from '../../assets/avatar1.jpg'
-import AVTR2 from '../../assets/avatar2.jpg'
-import AVTR3 from '../../assets/avatar3.jpg'
-import AVTR4 from '../../assets/avatar4.jpg'
+import data from '../../data/data.js'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
   return (
@@ -11,36 +18,26 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials_container">
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={AVTR1} alt="avatar 1" />
-          </div>
-          <h5 className="client_name">Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem distinctio magnam exercitationem cupiditate id quos autem aperiam quidem corrupti, explicabo omnis libero quo qui, accusamus similique quod pariatur, provident dolores!
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={AVTR1} alt="avatar 1" />
-          </div>
-          <h5 className="client_name">Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem distinctio magnam exercitationem cupiditate id quos autem aperiam quidem corrupti, explicabo omnis libero quo qui, accusamus similique quod pariatur, provident dolores!
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={AVTR1} alt="avatar 1" />
-          </div>
-          <h5 className="client_name">Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem distinctio magnam exercitationem cupiditate id quos autem aperiam quidem corrupti, explicabo omnis libero quo qui, accusamus similique quod pariatur, provident dolores!
-          </small>
-        </article>
-      
-      </div>
+      <Swiper className="container testimonials_container"
+      // install Swiper modules
+      modules={[ Pagination ]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+       {
+        data.testimonials.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={ avatar } alt={`avatar for ${name}`} />
+              </div>
+              <h5 className="client_name">{ name }</h5>
+              <small className="client_review">{ review }</small>
+            </SwiperSlide>
+          )
+        })
+       }
+      </Swiper>
     </section>
   )
 }
