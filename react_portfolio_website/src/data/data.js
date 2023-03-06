57e3dbd52e2824a5da58e18f50431c6bec4b2209
s3miOtics <colby.kauk@gmail.com>
2023-03-05 23:01:38 -0800

    add testimonials data

--- react_portfolio_website/src/data/data.js
+++ react_portfolio_website/src/data/data.js
@@ -1,6 +1,11 @@
 import IMG1 from "../assets/netflix_screenshot.png"
 
 
+import AVTR1 from '../assets/avatar1.jpg'
+import AVTR2 from '../assets/avatar2.jpg'
+import AVTR3 from '../assets/avatar3.jpg'
+import AVTR4 from '../assets/avatar4.jpg'
+
 const data = {
   projects: [
     {
@@ -46,6 +51,29 @@ const data = {
       website: 'https://github.com/s3mi0tics/MERN-NETFLIX-CLONE',
     },
   ],
+
+testimonials: [
+  {
+    avatar: AVTR1,
+    name: 'Ernest Achiever',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, exercitationem sunt! Porro quam magni nulla aut commodi debitis, quisquam recusandae suscipit atque repudiandae quia ipsa veniam quibusdam quas laboriosam soluta corrupti. Eum vel ullam repellat aut necessitatibus laboriosam qui enim.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Atta Whale',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labor distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labor distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labor distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+]
 };
 
 export default data;
