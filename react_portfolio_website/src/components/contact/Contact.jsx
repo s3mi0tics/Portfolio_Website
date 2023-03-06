a081db9ed51a931d14eff3e07e565aa3fd26adec
s3miOtics <colby.kauk@gmail.com>
2023-03-06 00:15:21 -0800

    add content to the contact component

--- react_portfolio_website/src/components/contact/Contact.jsx
+++ react_portfolio_website/src/components/contact/Contact.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { AiOutlineLinkedin } from 'react-icons/ai'
+import { BsWhatsapp } from 'react-icons/bs'
+import { LINKEDIN, WHATSAPP, EMAIL } from '../../utils/constants/socialLinks'
+
 
 const contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Tough</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+
+      {/* Contact Options */}
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>colby.kauk@gmail.com</h5>
+            <a href={`mailto:${EMAIL}`} target='_blank' rel="noreferrer">Email Me</a>
+          </article>
+          <article className="contact_option">
+            <AiOutlineLinkedin />
+            <h4>LinkedIn</h4>
+            <h5>Colby Kauk</h5>
+            <a href={ LINKEDIN } target='_blank' rel="noreferrer">Profile</a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>Colby Kauk</h5>
+            <a href={ WHATSAPP } target='_blank' rel="noreferrer">Send A Message</a>
+          </article>
+        </div>
+
+        {/* Form */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" placeholder="Your Message" rows="7" required></textarea>
+          <button type='submit'className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- react_portfolio_website/src/utils/constants/socialLinks.js
+++ react_portfolio_website/src/utils/constants/socialLinks.js
@@ -1,3 +1,4 @@
 export const LINKEDIN = "https://www.linkedin.com/in/colby-kauk/"
 export const GITHUB = "https://github.com/s3mi0tics"
 export const EMAIL = "colby.kauk@gmail.com"
+export const WHATSAPP = "https://api.whatsapp.com/send?phone=15302108568"
